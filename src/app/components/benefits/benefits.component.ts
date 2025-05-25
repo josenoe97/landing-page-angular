186ef576125bc5848f66525573761b04903a29f0
Junior <edsonjunior9701@outlook.com>
2025-05-25 18:29:31 -0300

    feat: criacao da section benefits

---
+++ src/app/components/benefits/benefits.component.ts
@@ -0,0 +1,14 @@
+import { NgOptimizedImage } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'benefits',
+  standalone: true,
+  imports: [    NgOptimizedImage
+  ],
+  templateUrl: './benefits.component.html',
+  styleUrl: './benefits.component.scss'
+})
+export class BenefitsComponent {
+
+}
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -3,11 +3,12 @@ import { HeaderComponent } from "../header/header.component";
 import { NgOptimizedImage } from '@angular/common';
 import { BtnPrimaryComponent } from "../btn-primary/btn-primary.component";
 import { NewsletterFormComponent } from "../newsletter-form/newsletter-form.component";
+import { BenefitsComponent } from "../benefits/benefits.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, NgOptimizedImage, BtnPrimaryComponent, NewsletterFormComponent],
+  imports: [HeaderComponent, NgOptimizedImage, BtnPrimaryComponent, NewsletterFormComponent, BenefitsComponent],
   providers: [],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
